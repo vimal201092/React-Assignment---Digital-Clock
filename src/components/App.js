373093d1cbd53c12755779228183637d3900552c
vimal201092 <vimalpal201092@gmail.com>
2021-01-19 15:27:38 +0530

    vimal

--- src/components/App.js
+++ src/components/App.js
@@ -34,19 +34,19 @@ class App extends Component {
         const currTime = this.state.time;
         const [hours,minutes,seconds] = [currTime.getHours(),currTime.getMinutes(),currTime.getSeconds()];
 
-       // const amOrPm = hours >= 12 ? "PM" : "AM";
-        //const twelveHourFormat = hours > 12 ? hours : hours;
-        //const hourString = this.padNumberToTwoDigits(hours);
-        //const minuteString = this.padNumberToTwoDigits(minutes);
-        //const secondString = this.padNumberToTwoDigits(seconds);
+        const amOrPm = hours >= 12 ? "PM" : "AM";
+        const twelveHourFormat = hours > 12 ? (hours-12) : hours;
+        const hourString = this.padNumberToTwoDigits(twelveHourFormat);
+        const minuteString = this.padNumberToTwoDigits(minutes);
+        const secondString = this.padNumberToTwoDigits(seconds);
 
-        const timeString = `${hours}:${minutes}:${seconds}`;
+        const timeString = `${hourString}:${minuteString}:${secondString} ${amOrPm}`;
         return timeString;
     }
 
-    //padNumberToTwoDigits(num) {
-    //  return `${ num < 10 ? "0" : ""}${num}`;
-    //}
+    padNumberToTwoDigits(num) {
+      return `${ num < 10 ? "0" : ""}${num}`;
+    }
 }
 
 
