3d66434d88a459f3f48374a1049bb96a815c3146
vimal201092 <vimalpal201092@gmail.com>
2021-01-19 15:19:29 +0530

    fin

--- src/components/App.js
+++ src/components/App.js
@@ -35,8 +35,8 @@ class App extends Component {
         const [hours,minutes,seconds] = [currTime.getHours(),currTime.getMinutes(),currTime.getSeconds()];
 
         const amOrPm = hours >= 12 ? "PM" : "AM";
-        const twelveHourFormat = hours > 12 ? hours : hours;
-        const hourString = this.padNumberToTwoDigits(twelveHourFormat);
+        //const twelveHourFormat = hours > 12 ? hours : hours;
+        const hourString = this.padNumberToTwoDigits(hours);
         const minuteString = this.padNumberToTwoDigits(minutes);
         const secondString = this.padNumberToTwoDigits(seconds);
 
